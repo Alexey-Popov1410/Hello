1bdb067d2864c129989a5e6da5ee61fa48185cc2
Alexey-Popov1410 <61269622+Alexey-Popov1410@users.noreply.github.com>
2020-02-22 16:44:10 +0300

    Create method for printing arguments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("HI!");
+        printArguments(args);
+    }
 
+    private static void printArguments(String[] args){
+        for (int i = 0; i < args.length; i++){
+            System.out.println(args[i]);
+        }
     }
 }
